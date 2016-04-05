10920ec2a791cfff8dc502c63a76535b7e20493d
anupait <anooprai790@gmail.com>
2016-04-05 23:12:08 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<script type="text/javascript">
+	$(function() {
+    		$("#testdiv").delay(6000).fadeOut(0);
+	});
+</script>
+
+
+
+
+if($_GET['emsg']==1)
+{
+?>
+<div id="testdiv" style="    
+  
+    background: #009427;
+    text-align: center;
+    color: #FFF;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    width: 782px;
+    margin: 0 auto;">You have successfully Updated your Profile  !!!</div>
+<?php } ?>
